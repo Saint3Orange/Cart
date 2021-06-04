c511edd5ae16ebb76cf1481d6d287ceb85a48169
SaintOrange <mike125125@mail.ru>
2021-06-04 17:02:44 +0700

    test

---
+++ database/migrations/2021_06_03_075802_sales.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Sales extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+           $table->id();
+           $table->increments('ID товара 1');
+           $table->increments('ID товара 2');
+           $table->string('Скидка на товар 1');
+           $table->string('Скидка на товар 2');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
